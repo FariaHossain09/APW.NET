7aaa2ad6fd599f66132bbe14abb8834ff7c48baa
Faria2000 <fariahocane2000@gmail.com>
2022-06-03 20:42:13 +0600

    first commit

---
+++ TaskCV/TaskCV/Controllers/CVController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TaskCV.Controllers
+{
+    public class CVController : Controller
+    {
+        // GET: CV
+        public ActionResult CVProfile()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult Edu()
+        {
+            return View();
+        }
+
+        public ActionResult Project()
+        {
+            return View();
+        }
+    }
+}
